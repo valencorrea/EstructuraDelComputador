1f253b1149875449dd6a8a60c249c4b223cf3d85
valen <vcorrea@fi.uba.ar>
2022-07-28 23:39:39 -0300

    add final sin fecha 1

---
+++ finales/sinFecha1/README.md
@@ -0,0 +1,3 @@
+# Sin fecha 1
+
+Subrutina que lee datos de la direccion de un periferico que recibe por stack, y devuelve el dato leido por la misma via. Lee de un dato a la vez. Cada dato tiene 32 bits y se compone de dos numeros en complemento a2. El primer numero es de 20 bits y el segundo de 12. Si el de 12 es positivo se guarda en un array de 16 posiciones declarado. El programa termina cuando el array esta lleno.
--- finales/sinFecha1/sinFecha1.asm
+++ finales/sinFecha1/sinFecha1.asm
@@ -16,29 +16,31 @@ add %r0, 16, %r6					! cant max vector
 add %r0, %r0, %r7					! indice de la posicion actual del vector
 
 	pop %r1						! guardo la direc del periferico
+	add %r0, array, %r9				! me guardo direc del array
 
 loop:
-	subcc %r6, %r5, %r0					! sigo teniendo lugar
+	subcc %r6, %r5, %r0				! sigo teniendo lugar
 	be exit						! se prende cuando es 0
 	ld %r1, %r2					! me guardo el valor del periferico 
-! asumimos que se va actualizando solo	
+							! asumimos que se va actualizando solo	
+
 	
-	sra %r2, 12, %r3					! me guardo las A
+	sra %r2, 12, %r3				! me guardo las A
 	
-sla %r2, 20, %r4
-sra %r4, 20, %r4					! me guardo las B	
+	sll %r2, 20, %r4
+	sra %r4, 20, %r4				! me guardo las B	
 
 	addcc %r4, %r0, %r0				! seteamos flags para el numero de 12
 	bneg loop
-	ld [array + %r7], %r4					! guardo el valor positivo
-	add %r7, 4, %r7					! avanzo de posicion
+	st %r4, %r9					! guardo el valor positivo en memoria ram
+	add %r9, 4, %r9					! avanzo de posicion
 	add %r5, 1, %r5					! aumento los guardados
 	ba loop
 
 exit:
 jmpl %r15, 4, %r0	
 
-.org 3000
+.org 48
 array:	.dwb 16	
-
 	.end
+
